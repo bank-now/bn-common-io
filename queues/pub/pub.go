d07377b0ef302d6d8c3a3a617792233735ff0387
Just1689 <Just1689@gmail.com>
2018-10-03 00:01:17 +0200

    Create producer

--- queues/pub/pub.go
+++ queues/pub/pub.go
@@ -1,1 +1,24 @@
 package pub
+
+import (
+	"flag"
+	"fmt"
+	"github.com/nsqio/go-nsq"
+)
+
+type Config struct {
+	Name    string
+	Version string
+	Address string
+	Topic   string
+}
+
+func Setup(c Config) (*nsq.Producer, error) {
+	cfg := nsq.NewConfig()
+	flag.Var(&nsq.ConfigFlag{cfg}, "producer-opt", "http://godoc.org/github.com/nsqio/go-nsq#Config")
+	flag.Parse()
+	cfg.UserAgent = fmt.Sprintf("%s-%s", c.Name, c.Version)
+
+	return nsq.NewProducer(c.Address, cfg)
+
+}
