e0c5a6173945d09a67730ec44acff96983462614
Just1689 <Just1689@gmail.com>
2018-10-07 16:18:26 +0200

    Post json

--- rest/http.go
+++ rest/http.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 )
@@ -12,4 +13,14 @@ func Get(url string) (body []byte, err error) {
 	}
 	defer response.Body.Close()
 	return ioutil.ReadAll(response.Body)
+}
+
+func Post(url string, b []byte) (body []byte, err error) {
+	response, err := http.Post(url, "application/json", bytes.NewBuffer(b))
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+	return ioutil.ReadAll(response.Body)
+
 }
