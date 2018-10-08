254ca1b2196b37d3ce68f4c86bb692a90c226ba8
Just1689 <Just1689@gmail.com>
2018-10-08 22:52:07 +0200

    Random hex string

---
+++ util/random.go
@@ -0,0 +1,20 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letters = []rune("abcdef0987654321")
+
+func RandomHexString(length int) string {
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
